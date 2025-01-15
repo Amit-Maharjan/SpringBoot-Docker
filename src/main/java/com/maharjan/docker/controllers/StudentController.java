435cbf6b7999862144e7b8d4fa7721e352b05726
Amit Maharjan <maharjanamit8@gmail.com>
2025-01-15 15:30:37 -0500

    Added 2 REST APIs to get and post the request from/to mysql database.

---
+++ src/main/java/com/maharjan/docker/controllers/StudentController.java
@@ -0,0 +1,25 @@
+package com.maharjan.docker.controllers;
+
+import com.maharjan.docker.models.Student;
+import com.maharjan.docker.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+}
